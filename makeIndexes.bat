22dac3d0bb9a47016e446d11a4318ca513d95a4a
Tim Twigg <tim@twiggusa.com>
2023-05-30 22:00:48 -0700

    play with weighting values

--- makeIndexes.bat
+++ makeIndexes.bat
@@ -1,11 +1,9 @@
 @ECHO OFF
 
 main.py --index -d test -op -b none
-MOVE summary.txt index/summary.txt
 RENAME index indexSmall
 ECHO.
 
 main.py --index -d large -op -b none
-MOVE summary.txt index/summary.txt
 RENAME index indexLarge
 ECHO.
